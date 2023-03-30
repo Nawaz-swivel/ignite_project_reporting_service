e66fbcab24a9df6a607f96244e7f6c87868eb412
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-03-30 09:18:19 +0530

    added header to swagger config

--- src/main/java/com/swivel/ignite/reporting/config/SwaggerConfig.java
+++ src/main/java/com/swivel/ignite/reporting/config/SwaggerConfig.java
@@ -2,15 +2,19 @@ package com.swivel.ignite.reporting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -19,6 +23,15 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
+        //Adding Header
+        ParameterBuilder aParameterBuilder = new ParameterBuilder();
+        List<Parameter> aParameters = new ArrayList<Parameter>();
+
+        aParameters.clear();
+
+        aParameterBuilder.name("Authorization").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        aParameters.add(aParameterBuilder.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(generateAPIInfo())
                 .select()
@@ -26,7 +39,9 @@ public class SwaggerConfig {
                 //specific package and include in the swagger documentation
                 .apis(RequestHandlerSelectors.basePackage("com.swivel.ignite.reporting"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+//                .ignoredParameterTypes(HeaderVo.class)
+                .globalOperationParameters(aParameters);
     }
 
     //Api information
