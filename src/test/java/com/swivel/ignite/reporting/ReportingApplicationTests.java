f62abf4b6baa1d21ca63e0ead76dab4924002653
Nawaz-swivel <nawas@swivelgroup.com.au>
2023-04-01 18:33:36 +0530

    added sonar scan to ci cd

--- src/test/java/com/swivel/ignite/reporting/ReportingApplicationTests.java
+++ src/test/java/com/swivel/ignite/reporting/ReportingApplicationTests.java
@@ -1,6 +1,9 @@
 package com.swivel.ignite.reporting;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * This class tests {@link ReportingApplication} class
@@ -11,10 +14,10 @@ class ReportingApplicationTests {
     /**
      * This method tests Spring application main run method
      */
-//	@Test
-//	void Should_RunSpringApplication() {
-//		ReportingApplication.main(new String[]{});
-//
-//		assertTrue(true, "Spring Application Context Loaded Successfully");
-//	}
+	@Test
+	void Should_RunSpringApplication() {
+		ReportingApplication.main(new String[]{});
+
+		assertTrue(true, "Spring Application Context Loaded Successfully");
+	}
 }
